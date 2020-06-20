bec2951978218f88f1e96c588de3784f4e3cd930
James Hockey <james@345.technology>
2020-06-20 09:07:43 +0100

    Char update

--- Test-Script.ps1
+++ Test-Script.ps1
@@ -2,14 +2,14 @@
 Write-Host "Starting"
 $startDate = Get-Date
 
-$endDate = Get-Date
-
-$duration = NEW-TIMESPAN –Start $startDate –End $endDate
-
 $packageName = "notepadplusplus"
 
 $latestPackageVersion = cmd.exe /c choco search $packageName --all --pre | Out-String -Stream | Select-String -Pattern "^$packageName.*$" | Sort-Object -Descending | Select-Object -First 1
   
 Write-Host $latestPackageVersion
+
+$endDate = Get-Date
+
+$duration = NEW-TIMESPAN -Start $startDate -End $endDate
 
 Write-Host "Ended, duration: $duration"
